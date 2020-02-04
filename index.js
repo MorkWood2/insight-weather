e1a5b3f17de7d85603523e5f43337ab3d3201892
Mark Wild <mark.wild189@gmail.com>
2020-02-03 16:21:28 -0800

    add styles and js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ window.addEventListener('load', () => {
   let temperatureDescription = document.querySelector(
     '.temperature-description'
   );
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
+  let temperatureSection = document.querySelector('.temperature');
+  const temperatureSpan = document.querySelector('.temperature span');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -22,8 +26,34 @@ window.addEventListener('load', () => {
         .then(data => {
           console.log(data);
           //es6 destructure
-          const { temperature, summary } = data.currently;
+          const { temperature, summary, icon } = data.currently;
+          //set Dom Elements from the API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContext = summary;
+          locationTimezone.textContent = data.timezone;
+          //Formula for celsius
+          let celsius = (temperature - 32) * (5 / 9);
+          //set Icon
+          setIcons(icon, document.querySelector('.icon'));
+
+          //change temperature to Celsius/Farenheit
+          temperatureSection.addEventListener('click', () => {
+            if (temperatureSpan.textContent === 'f') {
+              temperatureSpan.textContent = 'C';
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent === 'f';
+              temperatureDegree.textContent = temperature;
+            }
+          });
         });
     });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: 'white' });
+    const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
